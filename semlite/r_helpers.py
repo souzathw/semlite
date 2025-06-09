51f4876a58fd9fff59d2eaec60a19cd4a8335da2
Thiago Souza <thiagoawk@gmail.com>
2025-06-09 09:12:09 -0300

    Fix: ajustes no helpers

--- semlite/r_helpers.py
+++ semlite/r_helpers.py
@@ -1,12 +1,16 @@
-import rpy2.robjects as ro
-from rpy2.robjects import pandas2ri
 
-pandas2ri.activate()
+
+import rpy2.robjects as ro
+from rpy2.robjects.conversion import localconverter
+from rpy2.robjects import default_converter
+from rpy2.robjects import pandas2ri
 
 ro.r('library(lavaan)')
 
 def run_lavaan_sem(model_desc, df, estimator="WLSMV"):
-    r_df = pandas2ri.py2rpy(df)
+    with localconverter(default_converter + pandas2ri.converter):
+        r_df = pandas2ri.py2rpy(df)
+
     ro.globalenv['dados1'] = r_df
     ro.globalenv['modelo'] = model_desc
 
@@ -18,7 +22,8 @@ def run_lavaan_sem(model_desc, df, estimator="WLSMV"):
 
     indices = dict(zip(ro.r('names(indices)'), list(ro.r('indices'))))
 
-    estimates_r = ro.r('estimates')
-    estimates_df = pandas2ri.rpy2py(estimates_r)
+    with localconverter(default_converter + pandas2ri.converter):
+        estimates_r = ro.r('estimates')
+        estimates_df = pandas2ri.rpy2py(estimates_r)
 
     return indices, estimates_df
