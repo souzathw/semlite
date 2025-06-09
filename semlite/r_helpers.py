71cdaf9af73f4b0e2bb1609d7af54d56bfef58d5
Thiago Souza <thiagoawk@gmail.com>
2025-06-09 11:17:27 -0300

    Fix: helpers variaveis ordenadas

--- semlite/r_helpers.py
+++ semlite/r_helpers.py
@@ -7,14 +7,20 @@ from rpy2.robjects.pandas2ri import py2rpy, rpy2py
 ro.r('library(lavaan)')
 
 def run_lavaan_sem(model_desc, df, estimator="WLSMV"):
+ 
     with localconverter(default_converter + pandas2ri.converter):
         r_df = py2rpy(df)
 
+    
     ro.globalenv['dados1'] = r_df
     ro.globalenv['modelo'] = model_desc
 
+     
+    ordered_vars = [col for col in df.columns if col.upper().startswith("CULPA")]
+    ro.globalenv['ordered_vars'] = ro.StrVector(ordered_vars)
+ 
     ro.r(f'''
-    fit <- sem(model=modelo, data=dados1, ordered=colnames(dados1), estimator="{estimator}")
+    fit <- sem(model=modelo, data=dados1, ordered=ordered_vars, estimator="{estimator}")
     indices <- fitMeasures(fit, c("chisq", "df", "cfi", "tli", "rmsea", "rmsea.ci.lower", "rmsea.ci.upper", "srmr"))
     estimates <- parameterEstimates(fit, standardized=TRUE)
     ''')
@@ -22,7 +28,6 @@ def run_lavaan_sem(model_desc, df, estimator="WLSMV"):
     indices = dict(zip(ro.r('names(indices)'), list(ro.r('indices'))))
 
     estimates_r = ro.r('estimates')
-
     with localconverter(default_converter + pandas2ri.converter):
         estimates_df = rpy2py(estimates_r)
 
