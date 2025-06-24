0e8c7c60339cf4d6557624b3b0ce4bcbe4956912
Thiago Souza <thiagoawk@gmail.com>
2025-06-24 16:15:31 -0300

    Fix: correção convergencia r

--- semlite/r_helpers.py
+++ semlite/r_helpers.py
@@ -1,43 +1,37 @@
 import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
-from rpy2.robjects import default_converter
-from rpy2.robjects.pandas2ri import py2rpy, rpy2py_dataframe
+from rpy2.robjects.pandas2ri import rpy2py
 
+# Carrega o lavaan uma vez
 ro.r('library(lavaan)')
 
-def run_lavaan_sem(model_desc, csv_path, estimator="WLSMV", ordered_vars=None):
-    df = pandas2ri.read_csv(csv_path)
-
-    with localconverter(default_converter + pandas2ri.converter):
-        r_df = py2rpy(df)
-
-    ro.globalenv['dados1'] = r_df
+def run_lavaan_sem(model_desc, csv_path, estimator="ML", ordered_vars=None):
+    ro.r(f'dados1 <- read.csv("{csv_path}")')
+    ro.r('dados1 <- na.omit(dados1)') 
     ro.globalenv['modelo'] = model_desc
 
-    ro.r('dados1 <- na.omit(dados1)')
-
-    if ordered_vars is not None:
+    if ordered_vars:
         ro.globalenv['ordered_vars'] = ro.StrVector(ordered_vars)
         ro.r(f'''
-        fit <- sem(model=modelo, data=dados1, ordered=ordered_vars, estimator="{estimator}")
+            fit <- sem(model=modelo, data=dados1, estimator="{estimator}", ordered=ordered_vars)
         ''')
     else:
         ro.r(f'''
-        fit <- sem(model=modelo, data=dados1, estimator="{estimator}")
+            fit <- sem(model=modelo, data=dados1, estimator="{estimator}")
         ''')
 
     ro.r('''
-    indices <- fitMeasures(fit, c("chisq", "df", "cfi", "tli", "rmsea", "rmsea.ci.lower", "rmsea.ci.upper", "srmr"))
-    estimates <- parameterEstimates(fit, standardized=TRUE)
-    resumo <- capture.output(summary(fit, standardized=TRUE))
+        indices <- fitMeasures(fit, c("chisq", "df", "cfi", "tli", "rmsea", "rmsea.ci.lower", "rmsea.ci.upper", "srmr"))
+        estimates <- parameterEstimates(fit, standardized=TRUE)
+        resumo <- capture.output(summary(fit, fit.measures=TRUE, standardized=TRUE))
     ''')
 
-    indices = dict(zip(ro.r('names(indices)'), list(ro.r('indices'))))
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        estimates_df = rpy2py(ro.r('estimates'))
 
-    with localconverter(default_converter + pandas2ri.converter):
-        estimates_df = rpy2py_dataframe(ro.r('estimates'))
-        resumo = list(ro.r('resumo'))
+    indices = dict(zip(ro.r('names(indices)'), list(ro.r('indices'))))
+    resumo = list(ro.r('resumo'))
 
     return {
         "indices": indices,
