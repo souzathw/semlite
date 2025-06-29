d21230c2016ce1e0b12df38141bcc954b4f6c9d0
Thiago Souza <thiagoawk@gmail.com>
2025-06-29 19:27:19 -0300

    Fix: Correção deactivate

--- semlite/r_helpers.py
+++ semlite/r_helpers.py
@@ -2,12 +2,12 @@ import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 
-pandas2ri.activate()
 ro.r('library(lavaan)')
 
 def run_lavaan_sem(model_desc, df, estimator="WLSMV", ordered_vars=None):
     with localconverter(pandas2ri.converter):
         r_df = pandas2ri.py2rpy(df)
+
     ro.globalenv['dados1'] = r_df
     ro.globalenv['modelo'] = model_desc
 
@@ -16,12 +16,14 @@ def run_lavaan_sem(model_desc, df, estimator="WLSMV", ordered_vars=None):
         ordered_arg = 'ordered=ordered_vars,'
     else:
         ordered_arg = ''
+
     ro.r(f"""
     fit <- sem(model=modelo, data=dados1, {ordered_arg} estimator='{estimator}')
     indices <- fitMeasures(fit, c('chisq', 'df', 'cfi', 'tli', 'rmsea', 'rmsea.ci.lower', 'rmsea.ci.upper', 'srmr'))
     estimates <- parameterEstimates(fit, standardized=TRUE)
     resumo <- capture.output(summary(fit, standardized=TRUE))
     """)
+
     with localconverter(pandas2ri.converter):
         estimates_df = pandas2ri.rpy2py(ro.r('estimates'))
 
