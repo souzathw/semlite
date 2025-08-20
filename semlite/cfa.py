015ce29223c35772c329182f25033dae8db5ff80
souzathw <thiagoawk@gmail.com>
2025-08-20 07:01:59 -0300

    Fix: ajustes  modelo cfa

--- semlite/cfa.py
+++ semlite/cfa.py
@@ -1,6 +1,7 @@
 from semopy import ModelMeans
 from semopy.stats import calc_stats
 from semlite.utils import validar_csv, validar_variaveis, print_sucesso, carregar_arquivo_robusto
+import numpy as np
 
 def run_cfa(data_path, indicators):
     try:
@@ -9,15 +10,23 @@ def run_cfa(data_path, indicators):
 
         for lista in indicators.values():
             validar_variaveis(df, lista)
-
-        partes = [f"{fator} =~ {' + '.join(itens)}" for fator, itens in indicators.items()]
-        model_desc = "\n".join(partes)
+        model_desc = "\n".join([f"{fator} =~ {' + '.join(itens)}" for fator, itens in indicators.items()])
 
         model = ModelMeans(model_desc)
         model.fit(df)
 
         estimates = model.inspect(std_est=True)
-        fit_indices = calc_stats(model)
+        stats = calc_stats(model)
+        fit_indices = {
+            "Chi-Square": stats.chi2,
+            "df": stats.df,
+            "CFI": stats.cfi,
+            "TLI": stats.tli,
+            "RMSEA": stats.rmsea,
+            "RMSEA_low": stats.rmsea_low,
+            "RMSEA_high": stats.rmsea_high,
+            "SRMR": stats.srmr
+        }
 
         print_sucesso("CFA")
         print("📊 Resultados prontos para análise.")
